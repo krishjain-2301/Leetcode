2e72cfbc4d6468995af84941c47944a63a62b421
Krish Jain <jainkrish232006@gmail.com>
2026-08-22 22:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (37.42%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -5,9 +5,9 @@ public:
         int count=0;
         int pro=1;
         while(x>0){
-            int digit=x%10;
-            count+=digit;
-            pro*=digit;
+            int k=x%10;
+            count+=k;
+            pro*=k;
             x/=10;
         }
         return (n%(count + pro)==0);
