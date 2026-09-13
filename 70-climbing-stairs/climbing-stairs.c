92cb6db511b569cfd746b2de316d4a39e5a85deb
Krish Jain <jainkrish232006@gmail.com>
2026-09-14 01:15:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.47%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.c
@@ -0,0 +1,14 @@
+int climbStairs(int n) {
+    if(n==1)return 1;
+    if(n==2)return 2;
+    else{
+    int a = 1;
+    int b = 2;
+    for(int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+    }
+}
